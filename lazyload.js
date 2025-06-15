835d777f14d0173474abe4da82f0add8c83cadc7
lev-frontier <lev.frontier@gmail.com>
2025-06-15 12:09:53 +0800

    add new stoves, simpify img src, error fix

--- lazyload.js
+++ lazyload.js
@@ -1,15 +1,24 @@
 
-document.addEventListener("DOMContentLoaded", () => {
-	const images = document.querySelectorAll(".lazy-img");
+document.addEventListener("DOMContentLoaded",	()	=>	{
+	const	images	=	document.querySelectorAll(".lazy-img");
 
-	images.forEach((img) => {
-		const fullSrc = img.getAttribute("data-src");
-		const highRes = new Image();
-		highRes.src = fullSrc;
+	images.forEach((img)	=>	{
+		const	lowResSrc	=	img.getAttribute('src');
 
-		highRes.onload = () => {
-			img.src = fullSrc;
-			img.classList.add("loaded");
+		//	判斷要用	data-src	還是自動	replace
+		const	fullResSrc	=	img.getAttribute('data-src')	||	(
+				lowResSrc.includes('/small/') ? lowResSrc.replace('/small/',	'/') : null
+		);
+
+		if	(!fullResSrc)	return;	//	無法取得高解析圖就跳過
+
+		//	預先載入高解析圖
+		const	highRes	=	new	Image();
+		highRes.src	=	fullResSrc;
+
+		highRes.onload	=	()	=>	{
+				img.src	=	fullResSrc;
+				img.classList.add('loaded');
 		};
 	});
 });
